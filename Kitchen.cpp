9126b002d72462d5e0be3401f069cedfe2a7aa1b
Jerome Dobbels <jerome.dobbels@epitech.eu>
2017-12-08 14:53:58 +0100

    find name and size in Kitchen::Dispatch v2

--- Kitchen.cpp
+++ Kitchen.cpp
@@ -46,13 +46,13 @@ void Kitchen::dispatch(Team &aTeam) { //????
 		cookers.push_back(aTeam.takeCooker());
 	}
 	for (it = cookers.begin(); it != cookers.end(); ++it){
-                if (orders.front().find("XL") != std::string::npos){
+                if (orders.front().find("*XL") != std::string::npos){
                         size = "XL";
                         pizza = orders.front().substr(0,orders.front().size() -2);
-                } else if (orders.front().find("L") != std::string::npos){
+                } else if (orders.front().find("*L") != std::string::npos){
                         size = "L";
                         pizza = orders.front().substr(0,orders.front().size() -1);
-                }else if (orders.front().find("M")){
+                }else if (orders.front().find("*M")){
                         size = "M";
                         pizza = orders.front().substr(0,orders.front().size() -1);
                 }
